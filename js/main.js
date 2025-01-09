fa714bdef298e095fbb90c1fbf1dfebd1bf91963
soh003 <149380282+soh003@users.noreply.github.com>
2025-01-09 14:36:26 +0100

    addmethod added. frontend virker her

--- js/main.js
+++ js/main.js
@@ -5,8 +5,8 @@ const app = Vue.createApp({
             playgrounds: [],
             newPlayground:{
                 name:'',
-                MaxChildren: null,
-                MinAge: null,
+                maxChildren: null,
+                minAge: null,
             },
 
         }
@@ -22,6 +22,23 @@ const app = Vue.createApp({
                     console.log(error)
                 }
               )
+        },
+        AddMethod(){
+            const newPlayground = {
+                name: this.newPlayground.name,
+                maxChildren: this.newPlayground.maxChildren,
+                minAge: this.newPlayground.minAge,
+                
+            };
+
+            axios.post('https://playgroundrest20250109130538.azurewebsites.net/api/Playgrounds', newPlayground)
+            .then(Response=>{
+                console.log('Playground added', Response.data);
+                this.newPlayground.push(Response.data);
+            })
+            .catch(error=>{
+                console.log(error);
+            })
         }
     },
     computed:{
